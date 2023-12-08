c591ed345a4d0644feafd709d397ff06c4ec5d33
jishnusanthosh <jishnusanthosh321@gmail.com>
2023-12-08 21:08:06 +0530

    serviceCard Ui completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import About from '../components/About/About'
 import { Link } from 'react-router-dom'
 import { BsArrowRight } from 'react-icons/bs'
 import heroImg01 from '../assets/images/hero-img01.png'
@@ -8,6 +9,7 @@ import heroImg03 from '../assets/images/hero-img03.png'
 import icon01 from '../assets/images/icon01.png'
 import icon02 from '../assets/images/icon02.png'
 import icon03 from '../assets/images/icon03.png'
+import ServiceList from '../components/Services/ServiceList'
 
 const Home = () => {
   return (
@@ -147,15 +149,26 @@ const Home = () => {
                   World-class care for everyone. Our health System offers
                   unmatched, expert health care. From the lab clinic.{' '}
                 </p>
-                <Link
-                  to="/doctors"
-                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none "
-                >
-                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
-                </Link>
+                
               </div>
             </div>
           </div>
+        </div>
+      </section>
+
+      <About />
+      {/* {====== services section =========} */}
+      <section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center"> Our medical services</h2>
+            <p className="text__para text-center">
+              {' '}
+              World-class care for everyone.Our Health System Offers unmatched,
+              expert health care
+            </p>
+          </div>
+          <ServiceList />
         </div>
       </section>
     </>
