d9c07232aceca656121c1d69903a83895a793034
jishnusanthosh <jishnusanthosh321@gmail.com>
2023-12-08 14:36:44 +0530

    header UI completed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,18 +6,18 @@ export default {
   ],
   theme: {
     extend: {
-      primaryColor: "#0067FF",
-      yellowColor: "#FEB60D",
-      purpleColor: "#9771FF",
-      irisBlueColor: "#01B5C5",
-      headingColor: "#181A1E",
-      textColor: "#4E545F",
+      colors: {
+        primaryColor: "#0067FF",
+        yellowColor: "#FEB60D",
+        purpleColor: "#9771FF",
+        irisBlueColor: "#01B5C5",
+        headingColor: "#181A1E",
+        textColor: "#4E545F",
+      },
     },
-
     boxShadow: {
-      panelShadow: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;",
+      panelShadow: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px",
     },
   },
   plugins: [],
-}
-
+};
