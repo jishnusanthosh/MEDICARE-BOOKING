510f85d6cbebd57a2c4ecac9725260cf94762bc8
jishnusanthosh <jishnusanthosh321@gmail.com>
2023-12-17 21:04:39 +0530

    finalization

--- frontend/src/Dashboard/user-account/MyAccount.jsx
+++ frontend/src/Dashboard/user-account/MyAccount.jsx
@@ -1,4 +1,3 @@
-import userImg from '../../assets/images/doctor-img01.png'
 import { useContext, useState } from 'react'
 import { AuthContext } from '../../context/AuthContext'
 
@@ -8,7 +7,8 @@ import Profile from './Profile'
 import useGetProfile from '../../Hooks/useFetchData'
 import { BASE_URL } from '../../config'
 
-import Loading from '../../components/Loader/Loading'
+import Loading from '../../components/Loader/Loading';
+
 import Error from '../../components/Error/Error'
 
 function MyAccount() {
@@ -18,9 +18,6 @@ function MyAccount() {
   const { data: userData, loading, error } = useGetProfile(
     `${BASE_URL}/users/profile/me`,
   )
-
-  console.log(userData, 'userdata')
-
   const handleLogout = () => {
     dispatch({ type: 'LOGOUT' })
   }
@@ -28,10 +25,9 @@ function MyAccount() {
   return (
     <section>
       <div className="max-w-[1170px] px-5 mx-auto">
-        {loading &&!error  && <Loading />}
+        {loading && !error && <Loading />}
 
-        {!error &&!loading && <Error errMessage={error} />}
-
+        {!error && !loading && <Error errMessage={error} />}
 
         {!loading && !error && (
           <div className="grid md:grid-cols-3 gap-10">
@@ -39,7 +35,7 @@ function MyAccount() {
               <div className="flex item-center justify-center">
                 <figure className="w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor">
                   <img
-                    src={userImg}
+                    src={userData.photo}
                     alt=""
                     className="w-full h-full rounded-full"
                   />
@@ -47,15 +43,15 @@ function MyAccount() {
               </div>
               <div className="text-center mt-4">
                 <h3 className="text-[18px] leading-[30px] text-headingColor font-bold">
-                  Rahman
+                  {userData.name}
                 </h3>
                 <p className="text-textColor text-[15px] leading-6 font-medium">
-                  example@gmail.com
+                  {userData.email}
                 </p>
                 <p className="text-textColor text-[15px] leading-6 font-medium">
                   Blood Type:
                   <span className="ml-2 text-headingColor text-[22px] leading-8">
-                    A+
+                    {userData.bloodType}
                   </span>
                 </p>
               </div>
@@ -95,7 +91,7 @@ function MyAccount() {
               </div>
               {tab == 'booking' && <MyBooking />}
 
-              {tab == 'profile' && <Profile />}
+              {tab == 'profile' && <Profile user={userData} />}
             </div>
           </div>
         )}
